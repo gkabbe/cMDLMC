c8bbd273b20cf12a8e16239546ca1a7a8da19ffb
kabbe <gabriel.kabbe@chemie.uni-halle.de>
2015-09-04 17:19:34 +0200

    average_MC_out works more intelligently now. finds out line number and interval number on its own. choose between average (averages all observables such as msd, oh bond autocorrelation function and jumps) and slope (directly determines slope of msd)

--- mdkmc/kMC/average_MC_out.py
+++ mdkmc/kMC/average_MC_out.py
@@ -1,126 +1,145 @@
 #!/usr/bin/python
+# -*- coding: utf-8
 
 import numpy as np
-import argparse
+import argparse, argcomplete
 import ipdb, sys
 
-def avg(filename, lines, intervals, columns, time_columns, variance, verbose=False):
 
-	data=np.loadtxt(filename, usecols=columns)
-	time = np.loadtxt(filename, usecols=time_columns)
+def load_interval_samples(filename, lines, intervals, columns, time_columns):
+    data=np.loadtxt(filename, usecols=columns)
+    time = np.loadtxt(filename, usecols=time_columns)
 
-	if verbose == True:
-		print "#Shape of data:", data.shape
+    if verbose == True:
+        print("#Shape of data:", data.shape)
 
-	if intervals != None and intervals < data.shape[0]:
-		data = data[:intervals * lines]
-	else:
-		intervals = data.shape[0]/lines
+    if intervals != None and intervals < data.shape[0]:
+        data = data[:intervals * lines]
+    else:
+        intervals = data.shape[0]/lines
 
-	if verbose == True:
-		print "#Intervals:", intervals
+    if verbose == True:
+        print("#Intervals:", intervals)
 
-	data = data.flatten().reshape(intervals, lines, len(columns))
+    data = data.flatten().reshape(intervals, lines, len(columns))
+    
+    return data
+    
+def load_intervals_intelligently(filename):
+    data = np.loadtxt(filename, usecols=(0, 1, 2, 3, 4, 5, 6))
+    intervals = np.where(data[:, 2] == 0)[0]
+    interval_length = intervals[1]
+    with open(filename, "r") as f:
+        if "Averaged Results" in " ".join(f.readlines()):
+            interval_number = intervals.size - 1
+        else:
+            interval_number = intervals.size
+            
+    data = data[:interval_number*interval_length]
+    data = data.reshape((interval_number, interval_length, 7))
+    return data
 
-	avg = data.mean(axis=0)
 
-	if variance == True:
-		var = data[:,:,:].var(axis=0)
-		return time, avg, var
-	else:
-		return time[:avg.shape[0]], avg
+def avg(filename, variance, verbose=False):
 
+    data = load_intervals_intelligently(filename)
+    avg = data[:, :, 2:].mean(axis=0)
+    
+    time = data[0, :, 0:2]
+
+    if variance == True:
+        var = data[:,:,2:].var(axis=0)
+        return time, avg, var
+    else:
+        return time, avg
 
 
 def read_from_configfile(config_filename):
-	data = dict()
-	with open(config_filename, "r") as f:
-		for line in f:
-			if line.lstrip()[0] != "#":
-				if len(line.split()) > 2:
-					data[line.split()[0]] = line.split()[1:]
-				else:
-					data[line.split()[0]] = line.split()[1]
-		#~ for line in data:
-			#~ print line
-	return data
+    data = dict()
+    with open(config_filename, "r") as f:
+        for line in f:
+            if line.lstrip()[0] != "#":
+                if len(line.split()) > 2:
+                    data[line.split()[0]] = line.split()[1:]
+                else:
+                    data[line.split()[0]] = line.split()[1]
+    return data
+    
 
 def get_observable_names(outfilename):
-	with open(outfilename, "r") as f:
-		comments = [line for line in list(f) if line[0] == "#"]
-	return comments[-2][1:].split()
+    with open(outfilename, "r") as f:
+        comments = [line for line in list(f) if line[0] == "#"]
+        
+    for c in reversed(comments):
+        if "Sweeps" in c:
+            return c
+    
+#~ def bootstrap_msd(filename, fit_startpoint, samples=None):
+    #~ """Sample multiple times from the given data intervals, and each time determine the mean square displacement. 
+    #~ Return average MSD and standard deviation."""
+   #~ 
+    #~ data = load_interval_samples(filename, lines, intervals, columns, time_columns)
+    #~ if samples is None:
+        #~ samples = data.shape[0]
+    #~ rand_ints = np.random.randint(samples, size=(samples, data.shape[1]))
+    #~ bootstrap_data = data[rand_ints, np.arange(data.shape[1]), 2:5].sum(axis=2)
+    #~ 
+    #~ np.polyfit()
+    
+def get_slope(args):
+    """Fit model of the form f(x) = m * x + y to each interval.
+    Determine then mean and standard deviation of the resulting slopes."""
+
+    filename, fit_startpoint = args.file, args.msd_fitstart
+    data = load_intervals_intelligently(filename)
+    m, y = np.polyfit(data[0, fit_startpoint:, 1], data[:, fit_startpoint:, 2:5].sum(axis=2).T, 1)
+    m_mean, m_stddev = m.mean(), np.sqrt(m.var())
+    print("Slope in angström²/fs:")
+    print(m_mean, m_stddev)
+    print("Slope in pm²/ps:")
+    print(m_mean*1e7, m_stddev*1e7)
+    print("Diffusion coefficient in pm²/ps:")
+    print(m_mean*1e7/6, m_stddev*1e7/6)
+
+def average_kmc(args):
+    kmc_out = args.file
+    result = avg(kmc_out, args.variance, args.verbose)
+    
+    comments = get_observable_names(kmc_out)
+
+    if args.variance:
+        print("#{:12} {:12}"+6*" {:16}"+4*" {:8}".format(comments[0], comments[1], comments[2], comments[2]+"_var",  comments[3], comments[3]+"_var", comments[4], comments[4]+"_var", comments[5], comments[5]+"_var", comments[6], comments[6]+"_var"))
+        format_string = "{t[0]:10.2f} {t[1]:10.2f} {msd[0]:12.4f} {msdvar[0]:12.4f} {msd[1]:12.4f} {msdvar[1]:12.4f} {msd[2]:12.4f} {msdvar[2]:12.4f} {autocorr:6.2f} {autocorrvar:6.2f} {jumps:6.2f} {jumpsvar:6.2f}"
+        time, average, variance = result
+        
+        for i in range(average.shape[0]):
+            print(format_string.format(t=time[i], msd=average[i,0:3], msdvar=variance[i,0:3], autocorr=average[i,3], autocorrvar=variance[i,3], jumps=average[i,4], jumpsvar=variance[i,4]))
+    else:
+        print(comments)
+        print("#", " ".join(["{:<12}", "{:<12}", 3*"{:<16}", 2*"{:<8}"]).format(*comments))
+        format_string = "{t[0]:10.2f} {t[1]:10.2f} {msd[0]:12.4f} {msd[1]:12.4f} {msd[2]:12.4f} {autocorr:6.2f} {jumps:6.2f}"
+        time, average = result
+        for i in range(average.shape[0]):
+            print(format_string.format(t=time[i], msd=average[i,:3], autocorr=average[i,3], jumps=average[i,4]))
 
 def main(*args):
 
-	parser=argparse.ArgumentParser(description="Average KMC output. Assuming time in first column")
-	parser.add_argument("--file", help="KMC output")
-	parser.add_argument("--lines", type=int, help="Specify length of averaged interval")
-	parser.add_argument("--intervals", type=int, help="Average over how many intervals?")
-	parser.add_argument("--columns", "-c", type=int, nargs="+", help="Over which columns to average (starting with zero)")
-	parser.add_argument("--time_column", "-t", type=int, help="Specify time column")
-	parser.add_argument("--variance", action="store_true", help="Also output variance")
-	parser.add_argument("--verbose", "-v", action="store_true", help="Verbose")
-	parser.add_argument("--config_load", help="Config file of KMC run")
-	args = parser.parse_args()
-	
-	if args.config_load != None:
-		data = read_from_configfile(args.config_load)
-		try:
-			kmc_out = data["output"]
-		except KeyError:
-			print "#No kmc outputfile specified in kmc config"
-			if args.file == None:
-				print "#Neither is --file specified"
-				print "#Exiting"
-				sys.exit(1)
-			else:
-				print "#Using --file {}".format(args.file)
-				kmc_out = args.file
-			#~ ipdb.set_trace()
-		total_lines = int(data["sweeps"])/int(data["print_freq"])
-		intervals = int(data["sweeps"])/int(data["reset_freq"])
-		lines = total_lines / intervals
-		time_columns = 0, 1
-		columns = [2, 3, 4, 5, 6]
-		
-		result = avg(kmc_out, lines, intervals, columns, time_columns, args.variance, args.verbose)
+    parser=argparse.ArgumentParser(description="Average KMC output. Assuming time in first column")
+    parser.add_argument("--verbose", "-v", action="store_true", help="Verbose")
+    subparsers = parser.add_subparsers()
+    parser_slope = subparsers.add_parser("slope", help="Only determine slope of MSD")
+    parser_slope.add_argument("file", help="KMC output")
+    parser_slope.add_argument("--msd-fitstart", "-s", type=int, help="From which point in the MSD interval to start fitting")
+    parser_slope.set_defaults(func=get_slope)
+    parser_all = subparsers.add_parser("average", help="Average all columns from KMC output")
+    parser_all.add_argument("file", help="KMC output")
+    parser_all.add_argument("--variance", action="store_true", help="Also output variance")
+    parser_all.set_defaults(func=average_kmc)
+    argcomplete.autocomplete(parser)
+    args = parser.parse_args()
+    args.func(args)
+    
 
-	else:
-		kmc_out = args.file
-		result = avg(kmc_out, args.lines, args.intervals, args.columns, args.time_column, args.variance, args.verbose)
-	
-	comments = get_observable_names(kmc_out)
-	#~ print comments[0], comments[1],
-	#~ for column in columns:
-		#~ print comments[column],
-	#~ print""
-	
-	total_columns = len(time_columns) + len(columns)
-	if args.variance == True:
-		print "#{:12} {:12}"+6*" {:16}"+4*" {:8}".format(comments[0], comments[1], comments[2], comments[2]+"_var",  comments[3], comments[3]+"_var", comments[4], comments[4]+"_var", comments[5], comments[5]+"_var", comments[6], comments[6]+"_var")
-		format_string = "{t[0]:10.2f} {t[1]:10.2f} {msd[0]:12.4f} {msdvar[0]:12.4f} {msd[1]:12.4f} {msdvar[1]:12.4f} {msd[2]:12.4f} {msdvar[2]:12.4f} {autocorr:6.2f} {autocorrvar:6.2f} {jumps:6.2f} {jumpsvar:6.2f}"
-		t, a, v = result
-	
-		for i in xrange(a.shape[0]):
-			print format_string.format(t=t, msd=a[i,0:3], msdvar=v[i,0:3], autocorr=a[i,3], autocorrvar=v[i,3], jumps=a[i,4], jumpsvar=v[i,4])
-			#~ print t[i,0], t[i,1],
-			#~ for j in xrange(a.shape[1]):
-				#~ print a[i,j], v[i,j],
-			#~ print ""
-		
-	else:
-		print "#", " ".join(["{:<12}", "{:<12}", 3*"{:<16}", 2*"{:<8}"]).format(*comments)
-		format_string = "{t[0]:10.2f} {t[1]:10.2f} {msd[0]:12.4f} {msd[1]:12.4f} {msd[2]:12.4f} {autocorr:6.2f} {jumps:6.2f}"
-		t, a = result
-		#~ ipdb.set_trace()
-		for i in xrange(a.shape[0]):
-			print format_string.format(t=t[i], msd=a[i,:3], autocorr=a[i,3], jumps=a[i,4])
-			#~ print t[i,0], t[i,1],
-			#~ for j in xrange(a.shape[1]):
-				#~ print a[i,j],
-			#~ print ""
 
 if __name__ == "__main__":
-	main()
-
-
+    main()
