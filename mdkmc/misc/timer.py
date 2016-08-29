06acea6671bb8850fc500b8a8cd5ec141f555b3f
kabbe <gabriel.kabbe@chemie.uni-halle.de>
2016-08-29 17:31:14 +0200

    Remove unnecessary files

--- mdkmc/misc/timer.py
+++
@@ -1,9 +0,0 @@
-import time
-
-class TimeIt:
-	def __init__(self, f):
-		self.f = f
-	def __call__(self, *args):
-		start_time = time.time()
-		self.f(*args)
-		print("#Time:", time.time() - start_time)
