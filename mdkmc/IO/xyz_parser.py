2222f11fc2c12ca87c58c16c537f30d89c13a913
kabbe <gabriel.kabbe@chemie.uni-halle.de>
2016-08-30 17:10:48 +0200

    Try to increase speed when reading trajectory

--- mdkmc/IO/xyz_parser.py
+++ mdkmc/IO/xyz_parser.py
@@ -1,21 +1,24 @@
 #!/usr/bin/env python3
 
-import numpy as np
 import os
 import sys
-import ipdb
 import time
+import re
 
-from mdkmc.atoms import atomclass as ac
+import ipdb
+import numpy as np
+from numba import jit
+
 from mdkmc.atoms import numpyatom as npa
 from mdkmc.atoms.numpyatom import xyzatom as dtype_xyz
+from mdkmc.misc.tools import chunk
 
 
 class XYZFile(object):
 
     def __init__(self, filename, framenumber=None, verbose=False):
         self.filename = filename
-        self.datei = open(self.filename, "r")
+        self.datei = open(self.filename, "rb")
         self.atomnr = int(self.datei.readline().split()[0])
         self.atomdict = self.create_atomtypedict(verbose=verbose)
         self.datei.seek(0)
@@ -60,14 +63,15 @@ class XYZFile(object):
         # pdb.set_trace()
         if atomnames is None:
             atoms = np.zeros(self.atomnr, dtype=npa.xyzatom)
-            line = self.datei.readline()
-            if line == "":
-                raise EOFError
-            self.datei.readline()
-            for index in range(self.atomnr):
-                line = self.datei.readline()
-                atoms[index]["name"] = line.split()[0]
-                atoms[index]["pos"][:] = list(map(float, line.split()[1:4]))
+            # line = self.datei.readline()
+            # if line == "":
+            # raise EOFError
+            # self.datei.readline()
+            # for index in range(self.atomnr):
+            # line = self.datei.readline()
+            # atoms[index]["name"] = line.split()[0]
+            # atoms[index]["pos"][:] = list(map(float, line.split()[1:4]))
+            lines = self.datei.readlines()
         else:
             atomnr_selection = sum([len(self.atomdict[atomname]) for atomname in atomnames])
             atoms = np.zeros(atomnr_selection, dtype=npa.xyzatom)
@@ -108,18 +112,56 @@ class XYZFile(object):
 
         return np.array(traj, dtype=npa.xyzatom)
 
-    def get_trajectory_memmap(self, memmap_fname, atomnames=None, verbose=False):
+    def get_frame_numpy(self, f, chunks=None):
+        def filter_lines(f, frame_len):
+            for i, line in enumerate(f):
+                if i % frame_len not in (0, 1) and b"O" in line:
+                    yield line
+
+        data = np.genfromtxt(filter_lines(f, self.atomnr + 2), dtype=dtype_xyz,
+                             max_rows=chunks * 144).reshape((chunks, -1))
+
+        return data
+
+    def get_trajectory_memmap_old(self, memmap_fname, atomnames=None, verbose=False):
         frame_number = self.get_framenumber()
         memmap = np.lib.format.open_memmap(memmap_fname, dtype=dtype_xyz,
                                            shape=(frame_number, self.atomnr), mode="w+")
 
         start_time = time.time()
+        frames = []
         for i in range(frame_number):
-            memmap[i, :] = self.get_atoms_numpy(atomnames)
+            # memmap[i, :] = self.get_atoms_numpy(atomnames)
+            frames.append(self.get_atoms_numpy(atomnames))
             if verbose and i > 0 and i % 1000 == 0:
                 print("# {:06d} / {:06d} "
                       "... {:.1f} min remaining".format(i, frame_number,
                                                         (frame_number - i) / i *
+                                                        (time.time() - start_time)
+                                                        ),
+                      end="\r")
+                sys.stdout.flush()
+        if verbose:
+            print("")
+        memmap[:] = frames
+        return memmap
+
+    def get_trajectory_memmap(self, memmap_fname, atomnames=None, verbose=False):
+        if verbose:
+            print("# Determining number of frames of trajectory")
+        frame_number = self.get_framenumber()
+        memmap = np.lib.format.open_memmap(memmap_fname, dtype=dtype_xyz,
+                                           shape=(frame_number, 144), mode="w+")
+
+        start_time = time.time()
+        frames = []
+        self.datei.seek(0)
+        for start, stop in chunk(range(frame_number), 5000):
+            memmap[start:stop] = self.get_frame_numpy(self.datei, stop - start)
+            if verbose:
+                print("# {:06d} / {:06d} "
+                      "... {:.1f} min remaining".format(stop, frame_number,
+                                                        (frame_number - stop) / stop *
                                                         (time.time() - start_time)
                                                         ),
                       end="\r")
@@ -173,3 +215,77 @@ class XYZFile(object):
 
     def rewind(self):
         self.datei.seek(0)
+
+
+def get_positions(xyz_fname):
+    position_regex = r"""
+    ^\s*\w+             # atom name before positions
+    (\s*(?:(?:\-?\d+)?  # digit before decimal point
+    \.                  # decimal point
+    \d*\s*){3})$        # digits after decimal point"""
+
+    with open(xyz_fname, "r") as f:
+        atomnumber = int(f.readline())
+        f.seek(0)
+        first_frame = [f.readline() for i in range(atomnumber + 2)]
+        # offsets.append(len("".join(first_frame[:2])))
+        coordinates = "".join(first_frame[2:12])
+        print(coordinates)
+        offsets = re.findall(position_regex, coordinates, re.MULTILINE | re.VERBOSE)
+    return "\n".join(offsets)
+    # for match in offsets:
+    # print(coordinates[match.start(1):match.end(1)], match.start(1), match.end(1))
+
+
+# def parse(xyz_fname):
+#    regex = re.compile(r"""
+#    ^\s*(\w+)\s+
+#    (\-?\d*\.\d*   # 1st coordinate
+#    \s+
+#    \-?\d*\.\d*    # 2nd coordinate
+#    \s+
+#    \-?\d*\.\d*)$  # 3rd coordinate
+#    """, re.VERBOSE | re.MULTILINE)
+#
+#    with open(xyz_fname, "r") as f:
+#        print(re.findall(regex, f.read()))
+#        f.seek(0)
+#        atomnumber = int(f.readline())
+#        f.seek(0)
+#        arr = np.fromregex(f, regex, dtype=dtype_xyz)
+#
+#    return arr.reshape((-1, atomnumber))
+
+
+def parse(f, atomnr, chunk=None):
+    def filter_lines(f, frame_len):
+        for i, line in enumerate(f):
+            if i % frame_len not in (0, 1):
+                yield line
+
+    data = np.genfromtxt(filter_lines(f, atomnr + 2),
+                         dtype=dtype_xyz, max_rows=chunk * atomnr).reshape((-1, atomnr))
+
+    return data
+
+
+# @jit
+# def get_trajectory_numpy(trajectory, atomnames=None, verbose=False):
+#    start_time = time.time()
+#    traj = []
+#    counter = 0
+#    while 1:
+#        try:
+#            traj.append(self.get_atoms_numpy(atomnames))
+#        except EOFError:
+#            break
+#        if verbose and counter % 100 == 0:
+#            print("# Frame {}, ({:.2f} fps)".format(counter, float(
+#                counter) / (time.time() - start_time)), "\r", end=' ')
+#        counter += 1
+#    if verbose:
+#        print("")
+#    if verbose:
+#        print("# Total time: {} sec".format(time.time() - start_time))
+#
+#    return np.array(traj, dtype=npa.xyzatom)
