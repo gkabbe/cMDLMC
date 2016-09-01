7f7da9629f105d0ebce064a8e52589d6ee218473
kabbe <gabriel.kabbe@chemie.uni-halle.de>
2016-09-01 11:09:33 +0200

    Make CONFIG_DICT to OrderedDict

--- mdkmc/IO/config_parser.py
+++ mdkmc/IO/config_parser.py
@@ -1,4 +1,4 @@
-from collections import namedtuple
+from collections import OrderedDict
 from math import pi as PI
 import re
 import sys
@@ -50,100 +50,102 @@ def parse_bool(line):
         raise ValueError
 
 
-CONFIG_DICT = {
-    "filename":
+CONFIG_DICT = OrderedDict([
+    ("filename",
         {
             "parse_fct": parse_string,
             "default": "no_default",
-            "help": "Name of the coordinate file"
-        },
-    "output":
+            "help": "Name of the coordinate file. If it has the ending .xyz, a new file with the "
+                    "compressed coordinates and the ending .hd5 will be created."
+                    "The hdf5 file can also be specified directly."
+        }),
+    ("output",
         {
             "parse_fct": string2file,
             "default": sys.stdout,
             "help": "Name of the output file"
-        },
-    "o_neighbor":
+        }),
+    ("o_neighbor",
         {
             "parse_fct": parse_string,
             "default": "P",
             "help": "Name of the heavy atoms the oxygens are bonded to."
                     " Needed for the calculation of angle dependent jump rates."
-        },
-    "jumprate_type":
+        }),
+    ("jumprate_type",
         {
             "parse_fct": parse_string,
             "default": "no_default",
             "help": "Choose between jumprates determined from static DFT activation energy calculations (AE_rates) "
                     "and jumprates determined from AIMD simulations (MD_rates)."
-        },
-    "sweeps":
+        }),
+    ("sweeps",
         {
             "parse_fct": parse_int,
             "default": "no_default",
             "help": "Number of sweeps for the production run. A sweep is the number of single proton jump attempts, "
                     "after which (on average) each oxygen bond has been selected once."
-        },
-    "equilibration_sweeps":
+        }),
+    ("equilibration_sweeps",
         {
             "parse_fct": parse_int,
             "default": "no_default",
             "help": "Number of sweeps for the equilibration run."
-        },
-    "skip_frames":
+        }),
+    ("skip_frames",
         {
             "parse_fct": parse_int,
             "default": "no_default",
             "help": "How many frames to skip when updating the topology from the MD trajectory. A skip of zero frames "
                     "means that all frames are read from the trajectory."
-        },
-    "print_freq":
+        }),
+    ("print_freq",
         {
             "parse_fct": parse_int,
             "default": "no_default",
             "help": "After how many sweeps should information about the system be printed?"
-        },
-    "reset_freq":
+        }),
+    ("reset_freq",
         {
             "parse_fct": parse_int,
             "default": "no_default",
             "help": "After how many sweeps should quantities such as MSD and covalent bonding autocorrelation function "
                     "be reset? reset_freq should be a multiple of print_freq, in order to ease the averaging of the "
                     "final output."
-        },
-    "neighbor_search_radius":
+        }),
+    ("neighbor_search_radius",
         {
             "parse_fct": parse_float,
             "default": 15.0,
             "help": "All atoms whose distance is not larger than the neighbor_search_radius are considered as "
                     "neighbors."
-        },
-    "proton_number":
+        }),
+    ("proton_number",
         {
             "parse_fct": parse_int,
             "default": "no_default",
             "help": "The number of acidic protons the KMC algorithm will use."
-        },
-    "clip_trajectory":
+        }),
+    ("clip_trajectory",
         {
             "parse_fct": parse_int,
             "default": None,
             "help": "Clip the number of frames used from the trajectory. If not specified, the full trajectory will be "
                     "used."
-        },
-    "seed":
+        }),
+    ("seed",
         {
             "parse_fct": parse_int,
             "default": None,
             "help": "The seed for the random number generators. If none is specified, a random one will be chosen."
-        },
-    "md_timestep_fs":
+        }),
+    ("md_timestep_fs",
         {
             "parse_fct": parse_float,
             "default": None,
             "help": "Timestep of the used MD trajectory. Necessary to connect the KMC and MD time."
-        },
-    "angle_threshold":
+        }),
+    ("angle_threshold",
         {
             "parse_fct": parse_float,
             "default": PI / 2,
@@ -151,64 +153,64 @@ CONFIG_DICT = {
                     "jumprates will be set to zero. Theta is the angle between the vector connecting an oxygen and its "
                     "nearest heavy atom neighbor (whose type can be defined via \"o_neighbor\") and the vector "
                     "connecting the two oxygens between which the jumprate is determined. Default is ninety degrees."
-        },
-    "cutoff_radius":
+        }),
+    ("cutoff_radius",
         {
             "parse_fct": parse_float,
             "default": 4.0,
             "help": "Cutoff radius for the determination of jumprates. If two oxygens have a larger distance, the "
                     "jumprate will be set to zero."
-        },
-    "po_angle":
+        }),
+    ("po_angle",
         {
             "parse_fct": parse_bool,
             "default": True,
             "help": "Whether to use angle_dependent jumprates or not."
-        },
-    "shuffle":
+        }),
+    ("shuffle",
         {
             "parse_fct": parse_bool,
             "default": False,
             "help": "Whether to use shuffle mode, where frames from the trajectory are chosen randomly."
-        },
-    "verbose":
+        }),
+    ("verbose",
         {
             "parse_fct": parse_bool,
             "default": False,
             "help": "Turn verbosity on or off."
-        },
-    "xyz_output":
+        }),
+    ("xyz_output",
         {
             "parse_fct": parse_bool,
             "default": False,
             "help": "Print xyz output."
-        },
-    "periodic_wrap":
+        }),
+    ("periodic_wrap",
         {
             "parse_fct": parse_bool,
             "default": True,
             "help": "If true, proton motion will be wrapped into the periodic box."
-        },
-    "jumpmatrix_filename":
+        }),
+    ("jumpmatrix_filename",
         {
             "parse_fct": parse_string,
             "default": None,
             "help": "If a filename is given, the number of proton jumps between each oxygen pair will be counted and "
                     "saved."
-        },
-    "box_multiplier":
+        }),
+    ("box_multiplier",
         {
             "parse_fct": lambda line: list(map(int, line.split()[1:])),
             "default": [1, 1, 1],
             "help": "Extend the KMC box along one or more dimensions."
-        },
-    "pbc":
+        }),
+    ("pbc",
         {
             "parse_fct": get_pbc,
             "default": "no_default",
             "help": "Set the periodic boundary conditions of the MD trajectory."
-        },
-    "jumprate_params_fs":
+        }),
+    ("jumprate_params_fs",
         {
             "parse_fct": get_jumprate_parameters,
             "default": "no_default",
@@ -218,17 +220,17 @@ CONFIG_DICT = {
                     "the expected parameters are A, a, x0, xint and T. a, x0 and xint are fit parameters for the "
                     "function describing the activation energy over the oxygen distance, whereas A and T are parameters"
                     "of the Arrhenius equation, which converts the activation energy to a jumprate."
-        },
-    "higher_msd":
-    {
-            "parse_fct": parse_bool,
-            "default": False,
-            "help": "Calculates higher MSDs."
-            },
-    "var_prot_single":
+        }),
+    ("higher_msd",
         {
             "parse_fct": parse_bool,
             "default": False,
-            "help": "Calculate variance od proton MSD for single proton wise, not box wise."
-        }
-}
+            "help": "Calculates higher MSDs."
+        }),
+    ("var_prot_single",
+        {
+            "parse_fct": parse_bool,
+            "default": False,
+            "help": "Calculate variance of proton MSD for single proton wise, not box wise."
+        })
+])
