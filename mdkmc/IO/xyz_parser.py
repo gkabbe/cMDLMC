0356359b665434f5ee5334ba1737707b572dd500
kabbe <gabriel.kabbe@chemie.uni-halle.de>
2016-09-02 14:22:55 +0200

    Revise save_trajectory_to_hdf5

--- mdkmc/IO/xyz_parser.py
+++ mdkmc/IO/xyz_parser.py
@@ -36,39 +36,48 @@ def parse(f, frame_len, *atom_names, no_of_atoms=None, no_of_frames=None):
     data = np.genfromtxt(filter_, dtype=dtype_xyz)
     return data.reshape(output_shape)
 
-
-def save_trajectory_to_hdf5(xyz_fname, *atom_names, hdf5_fname=None, chunk=1000, verbose=False):
+def save_trajectory_to_hdf5(xyz_fname, *atom_names, only_acidic_protons=False, pbc=None, 
+                            hdf5_fname=None, chunk=1000, 
+                            remove_com_movement=False, verbose=False):
     with open(xyz_fname, "rb") as f:
         frame_len = int(f.readline()) + 2
         f.seek(0)
-        first_frame = parse(f, frame_len, no_of_frames=1)
-        atom_nr_dict = dict()
-        for name in atom_names:
-            atom_nr_dict[name] = (first_frame[0]["name"] == name).sum()
+        first_frame, = parse_xyz(f, frame_len, no_of_frames=1)
+        if "H" in atom_names and only_acidic_protons and pbc is not None:
+            acidic_proton_selection = np.array(npa.get_acidic_proton_indices(first_frame, pbc))
+            selections = dict(H=acidic_proton_selection)
+        else:
+            selections = dict()
+        for atom_name in atom_names:
+            atom_selection = np.where(first_frame["name"] == atom_name)[0]
+            selections[atom_name] = atom_selection
+            
+        all_indices = reduce(set.union, [set(s) for s in selections.values()])
 
-    a = tables.Atom.from_dtype(np.dtype("float32"))
+    a = tables.Atom.from_dtype(np.dtype("float"))
     filters = tables.Filters(complevel=5, complib="blosc")
     if not hdf5_fname:
         hdf5_fname = os.path.splitext(xyz_fname)[0] + ".hdf5"
 
     with tables.open_file(hdf5_fname, "a", filters=filters) as hdf5:
-        for name in atom_names:
-            hdf5.create_earray("/", name, atom=a, shape=(0, atom_nr_dict[name], 3))
+        hdf5.create_group("/", "trajectories")
+        for atom_name, indices in selections.items():
+            hdf5.create_earray("/trajectories", atom_name, atom=a, shape=(0, len(indices), 3))
 
         with open(xyz_fname, "rb") as f:
             counter = 0
             start_time = time.time()
             while True:
-                pos_all_atoms = parse(f, frame_len, *atom_names,
-                                      no_of_atoms=sum(atom_nr_dict.values()),
-                                      no_of_frames=chunk)
-                if pos_all_atoms.shape[0] == 0:
+                frames = parse_xyz(f, frame_len, no_of_frames=chunk)
+                if frames.shape[0] == 0:
                     break
-                for name in atom_names:
-                    pos_single_atom = pos_all_atoms[:, pos_all_atoms[0]["name"] == name]
-                    print(pos_single_atom.shape)
-                    hdf5.get_node("/", name).append(pos_single_atom["pos"])
-                    # print(hdf5.get_node("/", name))
+                if remove_com_movement:
+                    for frame in frames:
+                        npa.remove_com_movement_frame(frame)
+                for atom_name, indices in selections.items():
+                    hdf5.get_node("/trajectories", atom_name).append(frames[:, frames[0]["name"] == atom_name]["pos"])
                 counter += chunk
                 print("# Parsed frames: {:06d}. {:.2f} fps".format(
                     counter, counter / (time.time() - start_time)), end="\r")
+
+
