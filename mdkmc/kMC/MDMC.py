1b432ea9f20a3dacf89addce3ff574b583f963d4
kabbe <gabriel.kabbe@chemie.uni-halle.de>
2016-09-02 22:20:36 +0200

    Use npz files instead of hdf5

--- mdkmc/kMC/MDMC.py
+++ mdkmc/kMC/MDMC.py
@@ -157,36 +157,39 @@ def load_atoms(filename, auxiliary_file, clip, *atom_names, verbose=False):
     if filename:
         if auxiliary_file:
             if verbose:
-                print("# Both xyz file and auxiliary hdf5 file specified.")
+                print("# Both xyz file and auxiliary npz file specified.")
                 print("# Will try to load from auxiliary file", auxiliary_file)
             if not os.path.exists(auxiliary_file):
                 if verbose:
                     print("# Specified auxiliary file does not exist.")
                     print("# Creating it now...")
-                xyz_parser.save_trajectory_to_hdf5(filename, *atom_names, hdf5_fname=auxiliary_file)
-            return xyz_parser.load_trajectory_from_hdf5(auxiliary_file, *atom_names, clip=clip,
-                                                        verbose=verbose)
+                xyz_parser.save_trajectory_to_npz(filename, npz_fname=auxiliary_file,
+                                                  remove_com_movement=True)
+            return xyz_parser.load_trajectory_from_npz(auxiliary_file, *atom_names, clip=clip,
+                                                       verbose=verbose)
         else:
-            aux_fname = os.path.splitext(filename)[0] + ".hdf5"
+            aux_fname = os.path.splitext(filename)[0] + ".npz"
             if verbose:
                 print("# Only xyz file specified.")
                 print("# Looking for auxiliary file", fname, "...")
             if os.path.exists(aux_fname):
                 print("# Found it!")
-                return xyz_parser.load_trajectory_from_hdf5(aux_fname, *atom_names, clip=clip,
-                                                            verbose=verbose)
+                return xyz_parser.load_trajectory_from_npz(aux_fname, *atom_names, clip=clip,
+                                                           verbose=verbose)
             else:
                 if verbose:
                     print("# No auxiliary file found.")
                     print("# Will create it now...")
-                    xyz_parser.save_trajectory_to_hdf5(filename, *atom_names, hdf5_fname=aux_fname)
-                    return xyz_parser.load_trajectory_from_hdf5(aux_fname, *atom_names, clip=clip, verbose=verbose)
+                    xyz_parser.save_trajectory_to_npz(filename, npz_fname=aux_fname,
+                                                      remove_com_movement=True)
+                    return xyz_parser.load_trajectory_from_npz(aux_fname, *atom_names, clip=clip,
+                                                               verbose=verbose)
     else:
         print("# Found auxiliary file.")
         print("# Loading from there...")
         if auxiliary_file:
-            return xyz_parser.load_trajectory_from_hdf5(auxiliary_file, *atom_names, clip=clip,
-                                                        verbose=verbose)
+            return xyz_parser.load_trajectory_from_npz(auxiliary_file, *atom_names, clip=clip,
+                                                       verbose=verbose)
         else:
             raise InputError("Please specify either filename or auxiliary_file")
 
