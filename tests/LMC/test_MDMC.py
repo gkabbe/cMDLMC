482fa6bca93404807fd5fc418a0fb9903704e387
kabbe <gabriel.kabbe@chemie.uni-halle.de>
2016-10-04 22:15:47 +0200

    Update test_MDMC

--- tests/LMC/test_MDMC.py
+++ tests/LMC/test_MDMC.py
@@ -8,41 +8,21 @@ from mdlmc.LMC.MDMC import MDMC
 
 fake_output = StringIO()
 
-def fake_load_configfile(*args, **kwargs):
-    cfg = {"filename": "blub",
-         "o_neighbor": "P",
-         "auxiliary_file": None,
-         "clip_trajectory": 0,
-         "verbose": True,
-         "seed": 0,
-         "box_multiplier": [1, 1, 1],
-         "jumprate_params_fs": dict(A=0.07, a=0.1, b=0.2, c=0.3),
-         "md_timestep_fs": 0.5,
-         "pbc": "10 20 30",
-         "output": fake_output
-         }
-    return cfg
 
-
-def fake_load_atoms(*args, **kwargs):
-    return [np.random.random((1, 20, 3)) for i in range(len(args) - 1)]
+class FakeMDMC(MDMC):
+    def __init__(self):
+        self.oxygen_trajectory = np.random.random((1, 20, 3))
+        self.oxygennumber = 20
+        self.proton_number = 10
 
 
 class TestMDMC(unittest.TestCase):
-    @patch("mdlmc.LMC.MDMC.load_atoms", side_effect=fake_load_atoms)
-    @patch("mdlmc.LMC.MDMC.load_configfile", side_effect=fake_load_configfile)
-    def test_print_settings(self, fk_load_cfgf, fk_load_at):
-        mdmc = MDMC("bla")
-        mdmc.print_settings()
-        print(fake_output.getvalue())
 
-    @patch("mdlmc.LMC.MDMC.load_atoms", side_effect=fake_load_atoms)
-    @patch("mdlmc.LMC.MDMC.load_configfile", side_effect=fake_load_configfile)
-    def test_initialize_oxygen_lattice(self, fake_cfg, fake_atoms):
-        mdmc = MDMC("sth")
-        mdmc.proton_number = 10
+    def test_initialize_oxygen_lattice(self):
+        mdmc = FakeMDMC()
         oxy_lattice = mdmc.initialize_oxygen_lattice((1, 1, 1))
         self.assertEqual((oxy_lattice > 0).sum(), mdmc.proton_number)
+        self.assertTrue(oxy_lattice.sum() == int(mdmc.proton_number * (mdmc.proton_number + 1) / 2))
 
 
     def test_md_lmc_run(self):
