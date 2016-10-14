9e00b35c1d293a9cbae418b63f9c1f9eb5d74d92
kabbe <gabriel.kabbe@chemie.uni-halle.de>
2016-10-14 18:19:21 +0200

    Add tests for excess_charge_analyzer. Add option to visualize excess charge movement

--- mdlmc/analysis/excess_charge_analyzer.py
+++ mdlmc/analysis/excess_charge_analyzer.py
@@ -3,17 +3,29 @@ import argparse
 from mdlmc.IO import xyz_parser
 
 
+def excess_charge_collective_variable(oxygen_pos, proton_pos):
+    return proton_pos.sum(axis=0) - 2 * oxygen_pos.sum(axis=0)
+
+
 def main(*args):
     parser = argparse.ArgumentParser(
         description="Determine the excess charge movement in a water box",
         formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     parser.add_argument("trajectory", help="Trajectory in npz format")
+    parser.add_argument("--visualize", action="store_true", help="Visualize excess charge position")
     parser.add_argument("pbc", nargs=3, help="periodic boundary conditions")
     args = parser.parse_args()
 
     oxygens, protons = xyz_parser.load_trajectory_from_npz(args.trajectory, "O", "H")
+    if args.visualize:
+        atoms = xyz_parser.load_atoms(args.trajectory)
 
-    for oxygen_frame, proton_frame in zip(oxygens, protons):
+    for i, (oxygen_frame, proton_frame) in enumerate(zip(oxygens, protons)):
         # Like in the PVPA paper, create a collective variable that tracks the excess charge motion
-        excess_charge_colvar = proton_frame.sum(axis=0) - 2 * oxygen_frame.sum(axis=0)
-        print(" ".join(map(str, excess_charge_colvar)), flush=True)
+        excess_charge_colvar = excess_charge_collective_variable(oxygen_frame, proton_frame)
+        if args.visualize:
+            print(len(atoms[i]) + 1)
+            print()
+            for atom in atoms[i]:
+                print(atom["name"], " ".join(map(str, atom["pos"])))
+        print("S", " ".join(map(str, excess_charge_colvar)), flush=True)
---
+++ tests/analysis/test_excess_charge_analyzer.py
@@ -0,0 +1,46 @@
+import unittest
+import numpy as np
+
+from mdlmc.analysis.excess_charge_analyzer import  excess_charge_collective_variable
+
+class TestExcessChargeAnalyzer(unittest.TestCase):
+    def test_collective_variable_invariance(self):
+        """Assert that the collective variable is invariant for h2o movement"""
+        h2o_angle = np.radians(104.45)
+        h2o = np.array([[0, 0, 0],
+                        [0.96, 0, 0],
+                        [np.cos(h2o_angle) * 0.96, np.sin(h2o_angle) * 0.96, 0]])
+
+        colvars = []
+        for i in range(20):
+            waterbox = np.vstack([h2o + np.random.uniform(-20, 20, size=3) for i in range(20)])
+            oxygens = waterbox[::3]
+            protons = waterbox[np.array([i % 3 != 0 for i in range(waterbox.shape[0])])]
+            colvars.append(excess_charge_collective_variable(oxygens, protons))
+        for a, b in zip(colvars[:-1], colvars[1:]):
+            self.assertTrue(np.allclose(a, b))
+
+    def test_collective_variable_correlates_with_excess_charge(self):
+        """Assert that the collective variable actually tracks the excess charge movement"""
+        h2o_angle = np.radians(104.45)
+        h2o = np.array([[0, 0, 0],
+                        [0.96, 0, 0],
+                        [np.cos(h2o_angle) * 0.96, np.sin(h2o_angle) * 0.96, 0]])
+
+        waterbox = np.vstack([h2o + np.random.uniform(-20, 20, size=3) for i in range(20)])
+        oxygens = waterbox[::3]
+        protons = waterbox[np.array([i % 3 != 0 for i in range(waterbox.shape[0])])]
+        colvars = []
+        excess_charge_positions = []
+        for i in range(20):
+            excess_charge = np.random.uniform(-20, 20, size=3)
+            colvars.append(excess_charge_collective_variable(oxygens, np.vstack(
+                [protons, excess_charge[None, :]])))
+            excess_charge_positions.append(excess_charge)
+        colvars = np.array(colvars)
+        excess_charge_positions = np.array(excess_charge_positions)
+        for cv, ec in zip(colvars, excess_charge_positions):
+            cv_diff = cv - colvars[0]
+            ec_diff = ec - excess_charge_positions[0]
+            np.testing.assert_allclose(cv_diff, ec_diff)
+
