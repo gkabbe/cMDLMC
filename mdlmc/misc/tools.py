db7ae2b7c7739a2fd3280fa5a803e0dd3d872883
kabbe <gabriel.kabbe@chemie.uni-halle.de>
2016-10-20 10:26:15 +0200

    Add decorator argparse_compatible, which makes it possible to call a function with its standard arguments, or with the Namespace object from argparse
    Conflicts:
    	mdlmc/misc/tools.py

--- mdlmc/misc/tools.py
+++ mdlmc/misc/tools.py
@@ -1,5 +1,37 @@
+from functools import wraps
+import time
+import argparse
+import inspect
+
+
 def chunk(iterable, step):
     starts = range(0, len(iterable) - 1, step)
     stops = map(lambda x: min(x, len(iterable)), range(step, len(iterable) + step, step))
     for start, stop in zip(starts, stops):
         yield start, stop
+
+
+def timer(f):
+    @wraps(f)
+    def wrapper(*args, **kwargs):
+        start_time = time.time()
+        result = f(*args, **kwargs)
+        total_time = time.time() - start_time
+        print("# Total time for {}: {:.2f}".format(f.__name__, total_time))
+        return result
+
+    return wrapper
+
+
+def argparse_compatible(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        if len(args) == 1 and type(args[0]) == argparse.Namespace:
+            expected_args = inspect.signature(func).parameters.keys()
+            args = {arg: getattr(args[0], arg) for arg in expected_args if hasattr(args[0], arg)}
+            print(args)
+            return func(**args)
+        else:
+            return func(*args, **kwargs)
+
+    return wrapper
