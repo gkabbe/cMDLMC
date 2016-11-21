e3026166a4dae5104dc2df6f375e3f2ca58befd1
kabbe <gabriel.kabbe@chemie.uni-halle.de>
2016-11-21 10:57:48 +0100

    Add unit test of displacement calculation in cubic box

--- tests/LMC/test_ObservableHelper.py
+++ tests/LMC/test_ObservableHelper.py
@@ -1,5 +1,9 @@
 import unittest
-from mdlmc.cython_exts.LMC.PBCHelper import AtomBoxCubic, AtomBoxMonoclinic
+from unittest.mock import MagicMock
+
+import numpy as np
+from mdlmc.cython_exts.LMC.PBCHelper import AtomBoxCubic
+from mdlmc.LMC.MDMC import ObservableManager
 
 
 class TestObservableHelper(unittest.TestCase):
@@ -9,13 +13,27 @@ class TestObservableHelper(unittest.TestCase):
     def test(self):
         pass
 
-    def test_calculate_displacement(self):
+    def test_calculate_displacement_cubic(self):
+        """Assert correct displacement calculation in cubic box"""
         helper = MagicMock()
         helper.get_jumps.return_value = 42
-        atombox = AtomBoxCubic([10, 20, 30])
-        oxygen_trajectory = np.zeros((20, 3))
-        oxygen_trajectory[:, 1] = np.linspace(0, 19, 20)
+        atombox = AtomBoxCubic([4, 5, 8])
+        oxygen_trajectory = np.zeros((1, 20, 3))
+        oxygen_trajectory[0, :, 1] = np.linspace(0, 19, 20)
         oxygen_lattice = np.zeros(20, dtype=int)
-        print(oxygen_trajectory)
+        oxygen_lattice[0] = 1
         obsman = ObservableManager(helper, oxygen_trajectory, atombox, oxygen_lattice,
                                    proton_number=1, md_timestep=0.5, sweeps=1000)
+
+        pos = 0
+        for i in range(30):
+            print("snapshot before:", obsman.proton_pos_snapshot)
+            oxygen_lattice[pos] = 0
+            pos = (pos + 1) % 20
+            oxygen_lattice[pos] = 1
+            obsman.calculate_displacement(0)
+            print(obsman.displacement)
+            print(obsman.displacement == [0, 1.0, 0])
+            self.assertTrue((obsman.displacement == [0, i + 1, 0]).all())
+            print("snapshot afterwards:", obsman.proton_pos_snapshot)
+            print("displacement:", obsman.displacement)
