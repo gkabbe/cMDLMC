6d8c0dda8cfec01fe4eca3cc21cdc61b7762f1ea
kabbe <gabriel.kabbe@chemie.uni-halle.de>
2016-12-19 15:48:45 +0100

    Update excess_kmc

--- mdlmc/KMC/excess_kmc.py
+++ mdlmc/KMC/excess_kmc.py
@@ -33,17 +33,25 @@ def kmc_state_to_xyz(oxygens, protons, oxygen_lattice):
     print("S", " ".join([3 * "{:14.8f}"]).format(*oxygens[oxygen_index]))
 
 
+def fastforward_to_next_jump(probsums, proton_position, dt, frame):
+
+    time_selector = -np.log(np.random.random())
+    probabilities = probsums[:, proton_position] * dt
+
+    probabilities = np.roll(probabilities, -frame)
+    cumsum = np.cumsum(probabilities)
+    delta_frame = np.searchsorted(cumsum, time_selector)
+
+    return delta_frame
+
+
 @argparse_compatible
-def kmc(config_file):
+def kmc_main(config_file):
     settings = load_configfile(config_file, config_type="KMCWater")
     print_settings(settings)
 
     trajectory_fname = settings.filename
     verbose = settings.verbose
-    atombox = AtomBoxCubic(settings.pbc)
-    a, b = settings.jumprate_params_fs["a"], settings.jumprate_params_fs["b"]
-    jumprate_function = ExponentialFunction(a, b)
-    kmc = KMCRoutine(trajectory_fname, atombox, jumprate_function)
     chunk_size = settings.chunk_size
 
     hdf5_fname = get_hdf5_filename(trajectory_fname)
@@ -68,9 +76,6 @@ def kmc(config_file):
     proton_trajectory = create_dataset_from_hdf5_trajectory(hdf5_file, trajectory, "proton_trajectory",
                                                             proton_indices, chunk_size)
 
-    probsums = create_dataset_from_hdf5_trajectory(hdf5_file, oxygen_trajectory, "probability_sums",
-                                                   kmc.determine_probability_sums, chunk_size)
-
     trajectory_length = oxygen_trajectory.shape[0]
     t, dt = 0, settings.md_timestep_fs
     frame, sweep, jumps = 0, 0, 0
@@ -80,19 +85,21 @@ def kmc(config_file):
     oxygen_number = len(oxygen_indices)
     # Initialize with one excess proton
     oxygen_lattice = initialize_oxygen_lattice(oxygen_number, 1)
+    proton_position, = np.where(oxygen_lattice)[0]
+
+    atombox = AtomBoxCubic(settings.pbc)
+    a, b = settings.jumprate_params_fs["a"], settings.jumprate_params_fs["b"]
+    jumprate_function = ExponentialFunction(a, b)
+    kmc = KMCRoutine(trajectory_fname, atombox, oxygen_lattice, jumprate_function)
+
+    probsums = create_dataset_from_hdf5_trajectory(hdf5_file, oxygen_trajectory, "probability_sums",
+                                                   kmc.determine_probability_sums, chunk_size)
 
     output_format = "{:18d} {:18.2f} {:15.8f} {:15.8f} {:15.8f} {:10d}"
 
     while sweep < total_sweeps:
-        proton_position = np.where(oxygen_lattice)[0][0]
-        time_selector = -np.log(np.random.random())
-        prob_sum = 0
-        probabilities = probsums[:, proton_position] * dt
-        import ipdb; ipdb.set_trace()
 
-        probabilities = np.roll(probabilities, -frame)
-        cumsum = np.cumsum(probabilities)
-        delta_frame = np.searchsorted(cumsum, time_selector)
+        delta_frame = fastforward_to_next_jump(probsums, proton_position, dt, frame)
 
         for i in range(frame, frame + delta_frame):
             if xyz_output:
@@ -102,18 +109,14 @@ def kmc(config_file):
                 print(output_format.format(sweep, t, *oxygen_trajectory[i % trajectory_length,
                                                                         proton_position],
                                            jumps), flush=True, file=settings.output)
+
         frame = (frame + delta_frame) % trajectory_length
         sweep += delta_frame
         t += delta_frame * dt
-
         jumps += 1
-        transition_probs = # determine transitions for each oxygen pair
-        destination_indices = np.array(destination)[transition_indices]
-        event_selector = np.random.random() * transition_probs.sum()
-        transition_index = np.searchsorted(np.cumsum(transition_probs), event_selector)
-        oxygen_lattice[proton_position] = 0
-        proton_position = destination_indices[transition_index]
-        oxygen_lattice[proton_position] = 1
+
+        proton_position = kmc.determine_transition(oxygen_trajectory[frame])
+
         if xyz_output:
             kmc_state_to_xyz(oxygen_trajectory[frame], proton_trajectory[frame],
                              oxygen_lattice)
@@ -126,6 +129,6 @@ def main(*args):
     parser = argparse.ArgumentParser(description="KMC",
                                      formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     parser.add_argument("config_file", help="Config file")
-    parser.set_defaults(func=kmc)
+    parser.set_defaults(func=kmc_main)
     args = parser.parse_args()
     args.func(args)
