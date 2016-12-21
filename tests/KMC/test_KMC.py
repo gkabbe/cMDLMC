f7991c63fcb3db6a6308b0252403258713205062
kabbe <gabriel.kabbe@chemie.uni-halle.de>
2016-12-21 11:31:01 +0100

    Update test_fastforward_to_next_jump.
    * Test for different omegas and dts
    * Assert equality of time steps in constant rate KMC and variable rate KMC
    * Assert agreement of KMC time and MD time

--- tests/KMC/test_KMC.py
+++ tests/KMC/test_KMC.py
@@ -1,4 +1,5 @@
 import unittest
+from itertools import product
 
 import numpy as np
 
@@ -9,32 +10,37 @@ class TestKMC(unittest.TestCase):
 
     def test_fastforward_to_next_jump(self):
         """Compare the KMC with time-dependent rates with the KMC with constant rates"""
-        # Set a fixed rate of 0.06 / fs
-        omega = 0.06
-        probsums = np.ones((1000, 1)) * omega
-        sweep, frame, time = 0, 0, 0
-        jumps = 0
-        dt = 0.5  # fs
-        times = []
-        sweeps = []
-        np.random.seed(0)
-        for i in range(100):
-            delta_frame, delta_t = excess_kmc.ffjn(probsums, 0, dt, frame, time, 1000)
-            frame = (frame + delta_frame) % 100
-            sweep += delta_frame
-            time += delta_t
-            jumps += 1
-            times.append(time)
-            sweeps.append(sweep)
+        # Test for a selection of fixed rates (unit: fs^{-1})
+        omegas = [0.03, 0.06, 0.13]
+        dts = [0.1, 0.5, 1.3]  # fs
 
-        time = 0
-        times_verify = []
-        np.random.seed(0)
-        for i in range(100):
-            delta_t = -np.log(1 - np.random.random()) / omega
-            time += delta_t
-            times_verify.append(time)
+        # Test for different MD time steps
+        for dt, omega in product(dts, omegas):
+            probsums = np.ones((1000, 1)) * omega
+            sweep, frame, time = 0, 0, 0
+            times = []
+            sweeps = []
+            np.random.seed(0)
+            for i in range(100):
+                delta_frame, delta_t = excess_kmc.ffjn(probsums, 0, dt, frame, time, 1000)
+                frame = (frame + delta_frame) % 100
+                sweep += delta_frame
+                time += delta_t
+                times.append(time)
+                sweeps.append(sweep)
 
-        for i, (t1, t2, s) in enumerate(zip(times, times_verify, sweeps)):
-            print(i, t1, t2, s * dt)
-            self.assertAlmostEqual(t1, t2)
+            time = 0
+            times_verify = []
+            np.random.seed(0)
+            for i in range(100):
+                delta_t = -np.log(1 - np.random.random()) / omega
+                time += delta_t
+                times_verify.append(time)
+
+            print("KMC with variable rates, KMC with fixed rates, Trajectory steps")
+            for i, (t1, t2, s) in enumerate(zip(times, times_verify, sweeps)):
+                print(t1, t2, s)
+                # Assert equal times of both KMC methods
+                self.assertAlmostEqual(t1, t2)
+                # Assert agreement of KMC time and MD time
+                self.assertEqual(int(t1 // dt), s)
