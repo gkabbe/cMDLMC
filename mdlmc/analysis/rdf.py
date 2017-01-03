37094da12b0ba722b3457888f691fea93b2b6450
kabbe <gabriel.kabbe@chemie.uni-halle.de>
2017-01-03 18:38:58 +0100

    Refactor

--- mdlmc/analysis/rdf.py
+++ mdlmc/analysis/rdf.py
@@ -11,28 +11,49 @@ from mdlmc.IO import xyz_parser
 from mdlmc.misc.tools import argparse_compatible, chunk, chunk_trajectory
 
 
-def distance_histogram(trajectory_1, trajectory_2, atombox, histo_kwargs, verbose=False):
-    histogram = np.zeros(histo_kwargs["bins"], dtype=int)
+def calculate_histogram(traj_generator1, traj_generator2, atombox, dmin, dmax, bins, *,
+                        normalized=False, verbose=False):
 
+    if normalized:
+        pbc = np.array(atombox.periodic_boundaries)
+        maxlen = np.sqrt(((pbc / 2)**2).sum())
+        range_ = (0, maxlen)
+        max_bins = int(maxlen / (dmax - dmin) * bins)
+    else:
+        range_ = (dmin, dmax)
+        max_bins = bins
+    histogram = np.zeros(max_bins)
+    counter = 0
     start_time = time.time()
-    for i, (frame_1, frame_2) in enumerate(zip(trajectory_1, trajectory_2)):
-        dists = atombox.length_all_to_all(frame_1, frame_2)
-        if i % 1000 == 0 and verbose:
-            print("{:6d} ({:8.2f} fps)".format(i, float(i) / (time.time() - start_time)), end="\r",
-                  flush=True)
-        histo, edges = np.histogram(dists, **histo_kwargs)
-        histogram += histo
+    for (_, end, chk1), (_, _, chk2) in zip(traj_generator1, traj_generator2):
+        chk1 = np.array(chk1, order="C")
+        chk2 = np.array(chk1, order="C")
+        for frame_1, frame_2 in zip(chk1, chk2):
+            dists = atombox.length_all_to_all(frame_1, frame_2)
+            histo, edges = np.histogram(dists, bins=max_bins, range=range_)
+            if counter % 1000 == 0 and verbose:
+                print("{:8d} ({:8.2f} fps)".format(counter,
+                                                   float(counter) / (time.time() - start_time)),
+                      end="\r", flush=True)
+            histogram += histo
+            counter += 1
+
     dists = (edges[:-1] + edges[1:]) / 2
 
-    return histogram, dists, edges
+    if normalized:
+        histogram = np.array(histogram, dtype=float) / histogram.sum() / (edges[1] - edges[0])
+
+    return dists, edges, histogram
 
 
-def calculate_rdf(trajectory, selection1, selection2, atombox, histo_kwargs):
+def calculate_rdf(trajectory, selection1, selection2, atombox, dmin, dmax, bins, *, chunk_size=None,
+                  clip=None, verbose=False):
 
-    trj_gen1 = chunk_trajectory(trajectory, )
+    traj_gen1 = chunk_trajectory(trajectory, chunk_size, length=clip, selection=selection1)
+    traj_gen2 = chunk_trajectory(trajectory, chunk_size, length=clip, selection=selection2)
 
-    histo, dists, edges = distance_histogram(trajectory, selection1, selection2, atombox,
-                                             histo_kwargs)
+    dists, edges, histo = calculate_histogram(traj_gen1, traj_gen2, atombox, dmin, dmax, bins,
+                                              verbose=verbose)
 
     if (selection1 == selection2).all():
         n1, n2 = selection1.shape[0], selection1.shape[0] - 1
@@ -52,14 +73,11 @@ def calculate_rdf(trajectory, selection1, selection2, atombox, histo_kwargs):
     return rdf, dists
 
 
-@argparse_compatible
 def radial_distribution_function(trajectory, selection1, selection2, atombox, dmin, dmax,
                                  bins, *, clip=None, plot=False, verbose=False, chunk_size=None):
 
-    histo_kwargs = {"range": (dmin, dmax),
-                    "bins": bins}
-
-    rdf, dists = calculate_rdf(trajectory, selection1, selection2, atombox, histo_kwargs)
+    rdf, dists = calculate_rdf(trajectory, selection1, selection2, atombox, dmin, dmax, bins,
+                               chunk_size=chunk_size, clip=clip, verbose=verbose)
 
     if plot:
         plt.plot(dists, rdf)
@@ -73,93 +91,61 @@ def radial_distribution_function(trajectory, selection1, selection2, atombox, dm
 def calculate_distance_histogram(trajectory, selection1, selection2, atombox, dmin, dmax, bins, *,
                                  clip=None, plot=False, normalized=False, verbose=False,
                                  chunk_size=None):
-    if not chunk_size:
-        chunk_size = trajectory.shape[0]
-        verbose_histo = verbose
-    else:
-        verbose_histo = False
-
-    if normalized:
-        pbc = np.array(atombox.periodic_boundaries)
-        maxlen = np.sqrt(((pbc / 2)**2).sum())
-        range_ = (0, maxlen)
-        max_bins = int(maxlen / (dmax - dmin) * bins)
-    else:
-        range_ = (dmin, dmax)
-        max_bins = bins
-
     traj_gen1 = chunk_trajectory(trajectory, chunk_size, length=clip, selection=selection1)
     traj_gen2 = chunk_trajectory(trajectory, chunk_size, length=clip, selection=selection2)
 
-    histogram = np.zeros(max_bins)
+    dists, _, histogram = calculate_histogram(traj_gen1, traj_gen2, atombox, dmin, dmax, bins,
+                                              normalized=normalized, verbose=verbose)
 
-    counter = 0
-    start_time = time.time()
-    for (_, end, chk1), (_, _, chk2) in zip(traj_gen1, traj_gen2):
-        chk1 = np.array(chk1, order="C")
-        chk2 = np.array(chk1, order="C")
-        for frame_1, frame_2 in zip(chk1, chk2):
-            dists = atombox.length_all_to_all(frame_1, frame_2)
-            histo, edges = np.histogram(dists, bins=max_bins, range=range_)
-            if counter % 1000 == 0 and verbose:
-                print("{:8d} ({:8.2f} fps)".format(counter,
-                                                    float(counter) / (time.time() - start_time)),
-                      end="\r", flush=True)
-            histogram += histo
-            counter += 1
-
-    dists = (edges[:-1] + edges[1:]) / 2
-
-    if normalized:
-        histogram = np.array(histogram, dtype=float) / histogram.sum() / (edges[1] - edges[0])
     mask = np.logical_and(dmin <= dists, dists <= dmax)
-
     if plot:
         plt.plot(dists[mask], histogram[mask])
         plt.show()
 
     print("{:12} {:12}".format("Distance", "Probability"))
-    for d, h in zip(dists[mask], histogram[mask]):
+    for d, h in zip(dists, histogram):
         print("{:12.8f} {:12.8f}".format(d, h))
 
 
-def prepare_trajectory(args):
-    _, ext = os.path.splitext(args.file)
+@argparse_compatible
+def prepare_trajectory(file, pbc, bins, dmin, dmax, clip, elements, acidic_protons, verbose,
+                       plot, chunk_size, normalized, subparser_name):
+    _, ext = os.path.splitext(file)
     if ext == ".hdf5":
-        atom_names, trajectory = xyz_parser.load_trajectory_from_hdf5(args.file)
+        atom_names, trajectory = xyz_parser.load_trajectory_from_hdf5(file)
     else:
-        trajectory = xyz_parser.load_atoms(args.file, clip=args.clip)
+        trajectory = xyz_parser.load_atoms(file, clip=clip)
         atom_names = trajectory[0]["name"]
         trajectory = np.array(trajectory["pos"], order="C")
 
-    pbc = np.array(args.pbc)
+    pbc = np.array(pbc)
     atombox = AtomBoxCubic(pbc)
 
-    if args.acidic_protons and "H" in args.elements:
-        args.elements.remove("H")
-        acid_indices = npa.get_acidic_proton_indices(trajectory[0], atombox, verbose=args.verbose)
+    if acidic_protons and "H" in elements:
+        elements.remove("H")
+        acid_indices = npa.get_acidic_proton_indices(trajectory[0], atombox, verbose=verbose)
         selection1 = np.zeros(trajectory.shape[1], dtype=bool)
         selection1[acid_indices] = 1
     else:
-        selection1 = atom_names == args.elements[0]
-        args.elements.pop(0)
+        selection1 = atom_names == elements[0]
+        elements.pop(0)
 
-    if args.elements and args.elements[0] != "H":
-        selection2 = atom_names == args.elements[0]
+    if elements and elements[0] != "H":
+        selection2 = atom_names == elements[0]
     else:
         selection2 = selection1
 
-    if args.subparser_name == "rdf":
-        radial_distribution_function(trajectory, selection1, selection2, atombox, args.dmin,
-                                     args.dmax, args.bins, clip=args.clip, plot=args.plot,
-                                     verbose=args.verbose, chunk_size=args.chunk_size)
-    elif args.subparser_name == "histo":
-        calculate_distance_histogram(trajectory, selection1, selection2, atombox, args.dmin,
-                                     args.dmax, args.bins, clip=args.clip, plot=args.plot,
-                                     verbose=args.verbose, chunk_size=args.chunk_size,
-                                     normalized=args.normalized)
+    if subparser_name == "rdf":
+        radial_distribution_function(trajectory, selection1, selection2, atombox, dmin,
+                                     dmax, bins, clip=clip, plot=plot,
+                                     verbose=verbose, chunk_size=chunk_size)
+    elif subparser_name == "histo":
+        calculate_distance_histogram(trajectory, selection1, selection2, atombox, dmin,
+                                     dmax, bins, clip=clip, plot=plot,
+                                     verbose=verbose, chunk_size=chunk_size,
+                                     normalized=normalized)
     else:
-        raise RuntimeError("What is", args.subparser_name, "?")
+        raise RuntimeError("What is", subparser_name, "?")
 
 
 def main(*args):
@@ -181,8 +167,6 @@ def main(*args):
     subparsers = parser.add_subparsers(dest="subparser_name")
 
     parser_rdf = subparsers.add_parser("rdf", help="Determine radial distribution function")
-    parser_rdf.add_argument("--dmin", type=float, default=2.0, help="Minimal value")
-    parser_rdf.add_argument("--dmax", type=float, default=3.0, help="Maximal value")
     parser_rdf.set_defaults(func=prepare_trajectory)
 
     parser_histo = subparsers.add_parser("histo", help="Determine distance histogram")
