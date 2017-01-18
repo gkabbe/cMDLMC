8ed5a127d38745489d0ce69e1a90b30b33d47038
kabbe <gabriel.kabbe@chemie.uni-halle.de>
2017-01-18 17:19:00 +0100

    Write tests for generator class

--- tests/KMC/test_KMC.py
+++ tests/KMC/test_KMC.py
@@ -70,3 +70,51 @@ class TestKMC(unittest.TestCase):
         relative_error = (jumprate_avg - average) / average
         self.assertLessEqual(relative_error, 0.01, "The average jump rate deviates by more than"
                                                    "one percent!")
+
+    def test_trajectory_generator(self):
+        """Assert that the trajectory generator correctly yields the trajectory frames and
+        does repeat itself after a complete iteration"""
+        trajectory = np.zeros((5, 1, 3))
+        trajectory[:, 0, 0] = range(5)
+
+        trajgen = excess_kmc.trajectory_generator(trajectory)
+
+        for i in range(5):
+            self.assertEqual(i, next(trajgen)[0, 0])
+        for i in range(5):
+            self.assertEqual(i, next(trajgen)[0, 0])
+
+    def test_distance_generator(self):
+        """Assert t"""
+        distances = np.zeros((5, 1, 3))
+        distances[:, 0, 0] = 10
+        distances_rescaled = np.zeros((5, 1, 3))
+        distances_rescaled[:, 0, 0] = 5
+
+        def some_fct(x):
+            return np.where(x < 7, -11, 12)
+
+        params = ()
+
+        kmcgen = excess_kmc.KMCGen(oxy_idx=0, distances=distances,
+                                   distances_rescaled=distances_rescaled, jumprate_fct=some_fct,
+                                   jumprate_params=params)
+
+        distance_gen = kmcgen.distance_generator()
+
+        # Assert that distance_gen returns the rescaled distances if delay is not set
+        self.assertTrue(np.allclose(next(distance_gen), [5, 0, 0]))
+
+        # Assert that the delay results in larger distances, which will be scaled linearly down
+        # to the scaled distances
+        kmcgen.delay = 6
+        for i in range(6):
+            dist = next(distance_gen)
+            self.assertGreater(dist[0], distances_rescaled[0, 0, 0], "The distance should be"
+                                                                     "greater than the rescaled"
+                                                                     "distance")
+
+        # Assert that after the delay has passed, the distances are equal to the rescaled
+        # distances
+        for i in range(20):
+            self.assertEqual(next(distance_gen)[0], distances_rescaled[0, 0, 0])
