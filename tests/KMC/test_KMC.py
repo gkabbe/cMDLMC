fe2501109f14f27d168ecd877ff3ee094518590b
kabbe <gabriel.kabbe@chemie.uni-halle.de>
2017-01-20 16:47:52 +0100

    Update KMC tests

--- tests/KMC/test_KMC.py
+++ tests/KMC/test_KMC.py
@@ -1,12 +1,12 @@
 import unittest
-from itertools import product
+from itertools import product, cycle
 
 import numpy as np
 
 from mdlmc.KMC import excess_kmc
 
 
-class TestKMC(unittest.TestCase):
+class TestVariableJumpRateKMC(unittest.TestCase):
 
     def test_fastforward_to_next_jump(self):
         """Compare the KMC with time-dependent rates with the KMC with constant rates.
@@ -18,18 +18,19 @@ class TestKMC(unittest.TestCase):
 
         # Test for different MD time steps
         for dt, omega in product(dts, omegas):
-            probsums = np.ones((1000, 1)) * omega
+            def probsum_gen(omega):
+                while True:
+                    yield omega
+            jump_rates = probsum_gen(omega)
+            fastforward_gen = excess_kmc.fastforward_to_next_jump(jump_rates, dt)
             sweep, frame, time = 0, 0, 0
             times = []
             sweeps = []
             np.random.seed(0)
             for i in range(100):
-                delta_frame, delta_t = excess_kmc.fastforward_to_next_jump(probsums, 0, dt, frame,
-                                                                           time, 1000)
-                frame = (frame + delta_frame) % 100
+                frame, delta_frame, kmc_time = next(fastforward_gen)
                 sweep += delta_frame
-                time += delta_t
-                times.append(time)
+                times.append(kmc_time)
                 sweeps.append(sweep)
 
             time = 0
@@ -49,40 +50,65 @@ class TestKMC(unittest.TestCase):
                 # Assert agreement of KMC time and MD time
                 self.assertEqual(int(t1 // dt), s)
 
-    def test_variable_rates(self):
+    def test_variable_rates_average(self):
         """Make sinus-like jump rates and check average."""
         timesteps = np.linspace(0, 200 * np.pi, 10000)[:, None]
-        probsums = 0.06 + 0.02 * np.sin(timesteps)
-        average = np.mean(probsums)
+        jump_rates = 0.06 + 0.02 * np.sin(timesteps)
+        average = np.mean(jump_rates)
+        jump_rates = cycle(jump_rates)
+
         print(average)
         dt = 0.5
         counter = 0
-        frame, time = 0, 0
-        for i in range(100000):
-            delta_frame, delta_t = excess_kmc.fastforward_to_next_jump(probsums, 0, dt, frame, time,
-                                                                       timesteps.shape[0])
-            frame = (frame + delta_frame) % timesteps.shape[0]
-            time += delta_t
+        fastforward_gen = excess_kmc.fastforward_to_next_jump(jump_rates, dt)
+        for i in range(10000):
+            frame, delta_frame, kmc_time = next(fastforward_gen)
             counter += 1
-        jumprate_avg = counter / time
+        jumprate_avg = counter / kmc_time
         print("Number of jumps:", counter)
         print("Jump rate:", jumprate_avg)
         relative_error = (jumprate_avg - average) / average
         self.assertLessEqual(relative_error, 0.01, "The average jump rate deviates by more than"
                                                    "one percent!")
 
+    def test_variable_rates_index(self):
+        """Use variable rates that are all zero except for one entry.
+        Make sure that the jump always happens at that index"""
+        jumprate_length = 117
+        nonzero_index = 73
+        jump_rates = np.zeros(jumprate_length)
+        jump_rates[nonzero_index] = 0.17
+        jump_rates = cycle(jump_rates)
+        dt = 0.22
+
+        fastforward_gen = excess_kmc.fastforward_to_next_jump(jump_rates, dt)
+
+        for i, (frame, delta_frame, kmc_time) in enumerate(fastforward_gen):
+            print(frame % jumprate_length, delta_frame / jumprate_length)
+            if i > 0:
+                self.assertEqual(frame % jumprate_length, nonzero_index)
+            if i == 100:
+                break
+
+
+class TestGenerators(unittest.TestCase):
     def test_trajectory_generator(self):
         """Assert that the trajectory generator correctly yields the trajectory frames and
-        does repeat itself after a complete iteration"""
+        does repeat itself after a complete iteration.
+        Also check that the returned counter counts correctly"""
         trajectory = np.zeros((5, 1, 3))
         trajectory[:, 0, 0] = range(5)
 
         trajgen = excess_kmc.trajectory_generator(trajectory)
 
         for i in range(5):
-            self.assertEqual(i, next(trajgen)[0, 0])
+            counter, dist = next(trajgen)
+            self.assertEqual(i, dist[0, 0])
+            self.assertEqual(counter, i)
         for i in range(5):
-            self.assertEqual(i, next(trajgen)[0, 0])
+            counter, dist = next(trajgen)
+            self.assertEqual(counter, i + 5)
+            self.assertEqual(i, dist[0, 0])
 
     def test_distance_generator(self):
         """Assert t"""
@@ -103,18 +129,48 @@ class TestKMC(unittest.TestCase):
         distance_gen = kmcgen.distance_generator()
 
         # Assert that distance_gen returns the rescaled distances if delay is not set
-        self.assertTrue(np.allclose(next(distance_gen), [5, 0, 0]))
+        counter, dist = next(distance_gen)
+        self.assertTrue(np.allclose(dist, [5, 0, 0]))
 
         # Assert that the delay results in larger distances, which will be scaled linearly down
         # to the scaled distances
         kmcgen.delay = 6
         for i in range(6):
-            dist = next(distance_gen)
-            self.assertGreater(dist[0], distances_rescaled[0, 0, 0], "The distance should be"
-                                                                     "greater than the rescaled"
+            counter, dist = next(distance_gen)
+            self.assertGreater(dist[0], distances_rescaled[0, 0, 0], "The distance should be "
+                                                                     "greater than the rescaled "
                                                                      "distance")
 
         # Assert that after the delay has passed, the distances are equal to the rescaled
         # distances
         for i in range(20):
-            self.assertEqual(next(distance_gen)[0], distances_rescaled[0, 0, 0])
+            counter, dist = next(distance_gen)
+            self.assertEqual(dist[0], distances_rescaled[0, 0, 0])
+
+    def test_jumprate_generator(self):
+        distances = np.zeros((5, 1, 3))
+        distances[:, 0, 0] = 10
+        distances_rescaled = np.zeros((5, 1, 3))
+        distances_rescaled[:, 0, 0] = 5
+
+        def some_fct(x):
+            return np.where(x < 7, -11, 12)
+
+        params = ()
+
+        kmcgen = excess_kmc.KMCGen(oxy_idx=0, distances=distances,
+                                   distances_rescaled=distances_rescaled, jumprate_fct=some_fct,
+                                   jumprate_params=params)
+
+        probsum_gen = kmcgen.jumprate_generator()
+        for frame, probsum in probsum_gen:
+            print(frame, probsum)
+            if frame == 20:
+                break
+
+        probsum_gen = kmcgen.jumprate_generator()
+        kmcgen.delay = 20
+        for frame, probsum in probsum_gen:
+            print(frame, probsum)
+            if frame == 20:
+                break
