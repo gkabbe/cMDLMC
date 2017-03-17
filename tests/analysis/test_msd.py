d870346dc76402997e8342c57987793c9a806637
Christopher Peschel <christopher.peschel@student.uni-halle.de>
2017-03-17 14:52:00 +0100

    Write test function for calculate_msd
    Testing msd_mean and msd_var
    Test case is single particle moving [1, 1, 1] every step

---
+++ tests/analysis/test_msd.py
@@ -0,0 +1,30 @@
+import unittest
+import numpy as np
+
+from mdlmc.analysis.msd import calculate_msd
+from mdlmc.analysis.msd import displacement
+from mdlmc.cython_exts.LMC.PBCHelper import AtomBoxCubic
+
+
+class TestMSD(unittest.TestCase):
+    def test_msd_normal_msd_mean(self):
+        number_of_steps = 1000
+        trajectory = np.zeros((number_of_steps, 1, 3))
+        trajectory[:, 0, 0] = np.arange(number_of_steps)
+        trajectory[:, 0, 1] = np.arange(number_of_steps)
+        trajectory[:, 0, 2] = np.arange(number_of_steps)
+        pbc = np.array([1000, 1000, 1000])
+        intervalnumber = 100
+        intervallength = 200
+        msd_mean, msd_var = calculate_msd(trajectory, pbc, intervalnumber, intervallength)
+        self.assertAlmostEqual(msd_mean[100].mean(), 10000, delta=0.01)
+
+    def test_msd_normal_msd_var(self):
+        number_of_steps = 1000
+        trajectory = np.zeros((number_of_steps, 1, 3))
+        trajectory[:, 0, 0] = np.arange(number_of_steps)
+        pbc = np.array([1000, 1000, 1000])
+        intervalnumber = 100
+        intervallength = 200
+        msd_mean, msd_var = calculate_msd(trajectory, pbc, intervalnumber, intervallength)
+        self.assertAlmostEqual(msd_var.mean(), 0, delta=0.01)
