dc30eed8f8ba651acdfbde90f0dbfdbc9ec80901
kabbe <gabriel.kabbe@chemie.uni-halle.de>
2017-04-03 15:11:37 +0200

    Update output_analyzer
    * Rename hydronium_lifetime -> lifetime_histogram
    
    * Add function lifetime_autocorrelation

--- mdlmc/KMC/output_analyzer.py
+++ mdlmc/KMC/output_analyzer.py
@@ -7,7 +7,7 @@ from mdlmc.misc.tools import argparse_compatible
 
 
 @argparse_compatible
-def hydronium_lifetime(filename: str, bins: int, plot: bool = False):
+def lifetime_histogram(filename: str, bins: int, plot: bool = False, save: bool = False):
     last_number_of_jumps = 0
     last_jump_time = 0
     lifetimes = []
@@ -36,22 +36,59 @@ def hydronium_lifetime(filename: str, bins: int, plot: bool = False):
         plt.plot(centres, histo)
         plt.show()
 
-    np.savetxt("lifetime.txt", np.c_[centres, histo])
+    if save:
+        np.savetxt("lifetime.txt", np.c_[centres, histo])
 
-    with open("lifetime.txt", "a") as f:
-        print("# Average:", average, file=f)
-        print("# Standard Deviation:", stddev, file=f)
+        with open("lifetime.txt", "a") as f:
+            print("# Average:", average, file=f)
+            print("# Standard Deviation:", stddev, file=f)
+    else:
+        for c, h in zip(centres, histo):
+            print(c, h)
+        print("# Average:", average)
+        print("# Standard Deviation:", stddev)
+
+
+@argparse_compatible
+def lifetime_autocorrelation(filename: str, correlation_length: int, interval_distance: int):
+    print("# Loading data")
+    data = np.genfromtxt(filename, usecols=(5,), dtype=int)
+
+    correlated = np.zeros(correlation_length, dtype=int)
+    counter = 0
+
+    for i in range(0, data.size - correlation_length, interval_distance):
+        correlated += data[i] == data[i: i + correlation_length]
+        counter += 1
+
+    correlated = correlated.astype(float) / counter
+
+    for val in correlated:
+        print(val)
 
 
 def main():
     parser = argparse.ArgumentParser("Analyze hydronium lifetime",
                                      formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-    parser.add_argument("filename", help="KMC output file")
-    parser.add_argument("--bins", type=int, default=50, help="Number of bins")
-    parser.add_argument("--plot", action="store_true", help="Plot result")
+    subparsers = parser.add_subparsers()
+
+    histo = subparsers.add_parser("histogram", help="Calculate lifetime histogram")
+    histo.add_argument("filename", help="KMC output file")
+    histo.add_argument("--bins", type=int, default=50, help="Number of bins")
+    histo.add_argument("--plot", action="store_true", help="Plot result")
+    histo.add_argument("--save", action="store_true", help="Save result to file")
+    histo.set_defaults(func=lifetime_histogram)
+
+    autocorr = subparsers.add_parser("autocorr", help="Calculate lifetime autocorrelation")
+    autocorr.add_argument("filename", help="KMC output file")
+    autocorr.add_argument("correlation_length", type=int, help="Correlation length")
+    autocorr.add_argument("--interval_distance", "-d", default=20, type=int,
+                          help="Temporal distance between intervals")
+    autocorr.set_defaults(func=lifetime_autocorrelation)
+
     args = parser.parse_args()
 
-    hydronium_lifetime(args)
+    args.func(args)
 
 
 if __name__ == "__main__":
