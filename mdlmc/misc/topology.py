71e52f1e95b0ea62f53aaf1c1667b37c6faf2d3a
kabbe <gabriel.kabbe@chemie.uni-halle.de>
2018-02-09 21:35:08 +0100

    Add topology.py

---
+++ mdlmc/misc/topology.py
@@ -0,0 +1,38 @@
+import sys
+import ipdb
+import numpy as np
+from collections import Counter
+import argparse
+
+from mdlmc.IO import xyz_parser
+from mdlmc.cython_exts.LMC.LMCHelper import AtomBoxCubic, AtomBoxMonoclinic
+
+def main(*args):
+    parser = argparse.ArgumentParser(description="topology",
+                                     formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument("--pbc", nargs="+", type=float, help="PBC")
+    parser.add_argument("trajectory", help="Trajectory")
+    parser.add_argument("--selection", nargs="+", help="Atom names")
+    args = parser.parse_args()
+
+    fname = args.trajectory
+    selection = args.selection
+    c = Counter(selection)
+
+    pbc = np.array(args.pbc)
+    if len(pbc) == 3:
+        atom_box = AtomBoxCubic(pbc)
+    else:
+        atom_box = AtomBoxMonoclinic(pbc)
+
+    for k, v in c.items():
+        print("Looking for", v, "atoms of type", k)
+
+    frames = dict()
+    atom_types = list(c.keys())
+    atoms = xyz_parser.load_atoms(fname, *atom_types, clip=1)
+
+    for a, f in zip(atom_types, atoms):
+        frames[a] = f
+
+    ipdb.set_trace()
