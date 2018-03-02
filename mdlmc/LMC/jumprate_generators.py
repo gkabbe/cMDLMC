bca293279020b23ff6fb486686b8860063b210f7
kabbe <gabriel.kabbe@chemie.uni-halle.de>
2018-03-02 17:43:48 +0100

    add docstring for jumprate_generator

--- mdlmc/LMC/jumprate_generators.py
+++ mdlmc/LMC/jumprate_generators.py
@@ -1,4 +1,20 @@
+from typing import Iterable
+
+import numpy as np
 
 
-def jumprate_generator(trajectory_generator):
-    pass
+def jumprate_generator(trajectory_generator: Iterable[np.array]):
+    """
+    Converts frames of a trajectory into arrays consisting of
+    donor index, acceptor index, and jump probability
+
+    Parameters
+    ----------
+    trajectory_generator: Iterable[np.array]
+
+    Returns
+    -------
+    out: Iterable[np.array]
+    """
+    first_frame = next(trajectory_generator)
+
