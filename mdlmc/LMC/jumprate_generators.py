d319d551a08e204519e03d85ffe46b30a5e6cdcc
kabbe <gabriel.kabbe@chemie.uni-halle.de>
2018-03-03 19:47:56 +0100

    Update jumprate generator

--- mdlmc/LMC/jumprate_generators.py
+++ mdlmc/LMC/jumprate_generators.py
@@ -3,14 +3,17 @@ from typing import Iterator
 import numpy as np
 
 
-def jumprate_generator(trajectory_generator: Iterator[np.array]):
+def jumprate_generator_distance(trajectory_generator: Iterator[np.array]):
     """
     Converts frames of a trajectory into arrays consisting of
-    donor index, acceptor index, and jump probability
+    donor index, acceptor index, and jump probability.
+    In this implementation the jump rate depends only on the donor-acceptor
+    distance.
 
     Parameters
     ----------
     trajectory_generator: Iterator[np.array]
+        Trajectory of donor and acceptor positions
 
     Returns
     -------
