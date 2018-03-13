fdb1ac3d9b8d463f3bba904612d117fb0b1feaaa
kabbe <gabriel.kabbe@chemie.uni-halle.de>
2018-03-13 15:36:56 +0100

    Add test for NeighborTopology

---
+++ tests/atoms/test_atoms.py
@@ -0,0 +1,38 @@
+import logging
+
+import numpy as np
+
+from mdlmc.atoms.numpy_atom import NeighborTopology
+from mdlmc.cython_exts.LMC.PBCHelper import AtomBoxCubic
+
+
+logger = logging.getLogger(__name__)
+
+
+def test_NeighborTopology():
+    periodic_boundaries = [10, 10, 10]
+    atombox = AtomBoxCubic(periodic_boundaries)
+
+    atoms = np.array([[0.0, 0, 0],
+                      [1.5, 0, 0],
+                      [3.0, 0, 0],
+                      [6.0, 0, 0],
+                      [9.0, 0, 0]])
+
+    start       = [0,   0,   1,   1,   2,   4]
+    destination = [1,   4,   0,   2,   1,   0]
+    dist        = [1.5, 1.0, 1.5, 1.5, 1.5, 1.0]
+
+    atoms = atoms[None, ...]
+
+    cutoff = 2.0
+
+    top = NeighborTopology(iter(atoms), cutoff, atombox)
+
+    conn = top.get_topology_bruteforce()
+
+    for st_target, de_target, di_target, st, de, di in zip(start, destination, dist, conn.row,
+                                                           conn.col, conn.data):
+        assert st_target == st
+        assert de_target == de
+        assert di_target == di
