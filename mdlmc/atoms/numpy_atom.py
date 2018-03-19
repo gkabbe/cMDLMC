79e40d767d8d40a34f867a4cf8531c4e9410da75
kabbe <gabriel.kabbe@chemie.uni-halle.de>
2018-03-19 15:43:54 +0100

    Fix circular dependency

--- mdlmc/atoms/numpy_atom.py
+++ mdlmc/atoms/numpy_atom.py
@@ -7,7 +7,6 @@ import logging
 from scipy.sparse import lil_matrix
 
 from mdlmc.cython_exts.LMC.PBCHelper import AtomBox
-from ..IO.trajectory_parser import Trajectory
 
 
 logger = logging.getLogger(__name__)
@@ -128,7 +127,7 @@ class NeighborTopology:
     """Keeps track of the connections between atoms.
     Given a cutoff distance, for each atom the atoms within this
     distance will be determined."""
-    def __init__(self, trajectory: Trajectory, cutoff: float, atombox: AtomBox,
+    def __init__(self, trajectory, cutoff: float, atombox: AtomBox,
                  buffer: float = 0.0) -> None:
         """
         Parameters
