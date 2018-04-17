69d1775f8302174cc7081b9d19c1dfb230e6a9df
kabbe <gabriel.kabbe@chemie.uni-halle.de>
2018-04-17 14:50:22 +0200

    Use Fermi class

--- tests/integration/mdlmc_run.py
+++ tests/integration/mdlmc_run.py
@@ -9,16 +9,12 @@ from mdlmc.IO.trajectory_parser import XYZTrajectory
 from mdlmc.topo.topology import NeighborTopology
 from mdlmc.cython_exts.LMC.PBCHelper import AtomBoxCubic
 from mdlmc.LMC.MDMC import KMCLattice
-from mdlmc.LMC.output import xyz_output, observables_output
+from mdlmc.LMC.jumprate_generators import Fermi
 
 
 logger = daiquiri.getLogger(__name__)
 daiquiri.getLogger("mdlmc.topo").setLevel(daiquiri.logging.INFO)
 daiquiri.setup(level=daiquiri.logging.INFO)
-
-
-def fermi(x, a, b, c):
-    return a / (1 + np.exp((x - b) / c))
 
 
 def test_mdlmc():
@@ -29,11 +25,11 @@ def test_mdlmc():
     filename = "/home/kabbe/trajectories/400K.xyz"
     xyz_traj = XYZTrajectory(script_path / filename, selection="O", repeat=True, time_step=0.4)
 
-    jrf = partial(fermi, a=0.06, b=2.3, c=0.1)
+    jrf = Fermi(a=0.06, b=2.3, c=0.1)
     kmc = KMCLattice(xyz_traj, atom_box=atombox, lattice_size=144, proton_number=96,
                      jumprate_function=jrf, donor_atoms="O")
 
-    for frame_nr, time, msd, autocorr in observables_output(kmc, 1000, 10):
+    for frame_nr, time, msd, autocorr in kmc.observables_output(1000, 10):
         print(format.format(frame_nr, time, *msd, autocorr))
 
 
