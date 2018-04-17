43afbd1e93c76502d311b3ea0744543962a551ce
kabbe <gabriel.kabbe@chemie.uni-halle.de>
2018-04-17 10:31:13 +0200

    Add class CovalentAutoCorrelation

--- mdlmc/LMC/output.py
+++ mdlmc/LMC/output.py
@@ -3,6 +3,17 @@ import numpy as np
 from ..LMC.MDMC import KMCLattice
 
 
+class CovalentAutocorrelation:
+    def __init__(self, lattice):
+        self.update(lattice)
+
+    def update(self, lattice):
+        self.lattice = lattice
+
+    def calculate(self, lattice):
+        return np.sum((lattice == self.lattice) & (lattice != 0))
+
+
 def xyz_output(kmc: KMCLattice, particle_type: str = "H"):
     for f, t, frame in kmc:
         occupied = frame[kmc.occupied_sites]
