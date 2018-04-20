df88490865193ac739b514b49a71aa27be055931
kabbe <gabriel.kabbe@chemie.uni-halle.de>
2018-04-20 15:51:58 +0200

    Add jumprate function FermiAngle

--- mdlmc/LMC/jumprate_generators.py
+++ mdlmc/LMC/jumprate_generators.py
@@ -13,10 +13,21 @@ class Fermi:
         c:
             Width
         """
-        self.a = a
-        self.b = b
-        self.c = c
+        self._a = a
+        self._b = b
+        self._c = c
 
     def __call__(self, x):
-        return self.a / (1 + np.exp((x - self.b) / self.c))
+        return self._a / (1 + np.exp((x - self._b) / self._c))
+
+
+class FermiAngle(Fermi):
+    def __init__(self, a: float, b: float, c: float, theta: float):
+        super().__init__(a, b, c)
+        self._theta = theta
+
+    def __call__(self, x, theta):
+        if theta < np.pi / 2:
+            return 0
+        return super()(x)
 
