ae177cfa17e0cf58eabda88bba95f7cdd25a59dc
kabbe <gabriel.kabbe@chemie.uni-halle.de>
2018-04-25 16:44:02 +0200

    Update integration test

--- tests/integration/mdlmc_run.py
+++ tests/integration/mdlmc_run.py
@@ -5,7 +5,7 @@ import numpy as np
 import pytest
 
 from mdlmc.IO.trajectory_parser import XYZTrajectory, HDF5Trajectory
-from mdlmc.topo.topology import NeighborTopology, AngleTopology
+from mdlmc.topo.topology import NeighborTopology, AngleTopology, HydroniumTopology
 from mdlmc.cython_exts.LMC.PBCHelper import AtomBoxCubic
 from mdlmc.LMC.MDMC import KMCLattice
 from mdlmc.LMC.jumprate_generators import Fermi, FermiAngle
@@ -40,7 +40,8 @@ def jumprate_function(request):
 
 
 @pytest.fixture(params=[(NeighborTopology, {}),
-                        (AngleTopology, {"extra_atoms": "P", "group_size": 3})])
+                        (AngleTopology, {"extra_atoms": "P", "group_size": 3}),
+                        (HydroniumTopology, {})])
 def topology(request, trajectory, atombox):
     Topo, extra_params = request.param
     return Topo(trajectory, atombox, donor_atoms="O", cutoff=3.0, buffer=2.0, **extra_params)
@@ -50,13 +51,18 @@ def topology(request, trajectory, atombox):
                          [("xyz_output", {}), ("observables_output", {"reset_frequency": 100,
                                                                       "print_frequency": 10})])
 def test_mdlmc(topology, atombox, jumprate_function, output_type, output_params):
-    if (type(topology), type(jumprate_function)) not in [(NeighborTopology, Fermi), AngleTopology, FermiAngle]:
+    if (type(topology), type(jumprate_function)) not in [(NeighborTopology, Fermi),
+                                                         (AngleTopology, FermiAngle),
+                                                         (HydroniumTopology, Fermi)]:
         pytest.skip(f"{topology.__class__} and {jumprate_function.__class__} not compatible.")
+
+    logger.info("Topology: %s, Jumprate: %s", topology, jumprate_function)
 
     kmc = KMCLattice(topology, atom_box=atombox, lattice_size=144, proton_number=96,
                      jumprate_function=jumprate_function, donor_atoms="O", time_step=0.4)
 
     for frame in getattr(kmc, output_type)(**output_params):
+        if output_type != "xyz_output":
             print(frame)
 
 
